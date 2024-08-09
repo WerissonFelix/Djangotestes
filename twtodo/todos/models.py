40924a3cde1dea3b0c745e9e6bd8b1dee6b6ed7e
Werisson  Félix <151643029+WerissonFelix@users.noreply.github.com>
2024-08-09 20:51:39 -0300

    Add files via upload

---
+++ twtodo/todos/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now=True, null=False, blank=False)
+    deadline = models.DateTimeField(null=False, blank=False)
+    finished_at = models.DateTimeField(null=True)
+
+
+class alunos(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    ano = models.DateTimeField(auto_now=True, null=False, blank=False)
+    idade = models.DateTimeField(null=False, blank=False)
+    matricula = models.DateTimeField(null=True)
