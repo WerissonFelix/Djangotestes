13c22a34246199099a47559375aba092cad6362f
Werisson  Félix <151643029+WerissonFelix@users.noreply.github.com>
2024-08-12 20:43:25 -0300

    Add files via upload

--- twtodo/todos/models.py
+++ twtodo/todos/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(verbose_name= "titulo",max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now=True, null=False, blank=False)
-    deadline = models.DateTimeField(null=False, blank=False)
+    deadline = models.DateTimeField(verbose_name="data de entrega",null=False, blank=False)
     finished_at = models.DateTimeField(null=True)
 
 
--- twtodo/todos/views.py
+++ twtodo/todos/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 
+from django.views.generic import ListView, CreateView
+from  todos.models import Todo
+from django.urls import reverse_lazy
 
-def home(request):
-    return render(
-        request,
-        "todos/home.html",
-    )
+class TodoListView(ListView):
+    model = Todo
+   
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"] # o que o usuário vai add no db
+    success_url = reverse_lazy("todo_list") # para onde a view irá dps da adição
+    
