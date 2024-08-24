a71e1a05426c820b5ab42b6584290e8f25c2de05
Werisson  Félix <151643029+WerissonFelix@users.noreply.github.com>
2024-08-23 22:32:40 -0300

    Add files via upload

--- cactus/setup/urls.py
+++ cactus/setup/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import index
+from todos.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index),
+    path("", TodoListView.as_view(), name = 'listar'),
+    path("create", TodoCreateView.as_view(), name= 'adicionar'),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name='atualizar'),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name='deletar'),
 ]
--- cactus/todos/views.py
+++ cactus/todos/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from todos.models import *
 
+class TodoListView(ListView):
+    model = Empresas  
 
+class TodoCreateView(CreateView):
+    model = Empresas
+    fields = ["id","nome","email", "senha", "jogo", "logo", "desc"]
+    success_url = reverse_lazy("listar")
 
-def index(request):
-    return render(
-        request,
-        "todos/index.html",
-    )
+class TodoUpdateView(UpdateView):
+    model = Empresas
+    fields = ["id","nome","email", "senha", "jogo", "logo", "desc"]
+    success_url = reverse_lazy("listar")
+
+class TodoDeleteView(DeleteView):
+    model = Empresas
+    success_url = reverse_lazy("listar")
